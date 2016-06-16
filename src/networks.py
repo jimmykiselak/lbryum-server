6ea9d999dc75d3caa019e6238b60286ebe8b5b7e
Jack <jack@robisonservice.com>
2016-06-15 22:09:56 -0400

    install prereqs in configure

--- src/networks.py
+++ src/networks.py
@@ -2,9 +2,13 @@
 
 params = {
     'bitcoin_main': {
+        'pubkey_address': 0,
+        'script_address': 5,
         'genesis_hash': '4696e50ae611caacea3c4d1f69484137628240bfd972bcd71d1520b93866dc98'
     },
     'bitcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
         'genesis_hash': 'ddfca77cf52e94b63ae05013e97af9db05538ae257fec15b7e234860e94daba1'
     }
 }
